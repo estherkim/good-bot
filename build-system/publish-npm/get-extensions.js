771c9b1c9ea54bc85e9d4a29e097ad23dd73aeba
esth <esth@google.com>
2021-05-19 11:54:03 -0400

    debug

--- build-system/publish-npm/get-extensions.js
+++ build-system/publish-npm/get-extensions.js
@@ -1,3 +1,3 @@
 const bundles = require('../compile/bundles.config.extensions.json');
-const extensionsToPublish = bundles.filter(bundle => bundle.options?.npm).map(bundle => bundle.name);
+const extensionsToPublish = bundles.filter(bundle => bundle.options?.npm).map(bundle => ({"extension": bundle.name }));
 console.log(JSON.stringify(extensionsToPublish));
