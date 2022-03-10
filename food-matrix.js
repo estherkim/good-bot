2930de2bd73a36b45b2421c694632bac7c0bd0ca
Esther Kim <44627152+estherkim@users.noreply.github.com>
2022-03-10 15:27:53 -0500

    Update food-matrix.js

--- food-matrix.js
+++ food-matrix.js
@@ -1,4 +1,5 @@
 const fruits = [{color: 'red', fruit: 'apple'}, {color: 'red', fruit: 'plum'}];
 const veggies = [{color: 'pink', veggie: 'onion'}, {color: 'pink', veggie: 'tree'}];
-process.env.FRUITS = JSON.stringify(fruits);
-process.env.VEGGIES = JSON.stringify(veggies);
+
+const all = {fruits, veggies};
+process.stdout.write(JSON.stringify(all));
